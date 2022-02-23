f35ff0eb6ff236c42e21dbcd6be9f41b76af219f
kato-shinya <tokatoka.it.777@gmail.com>
2022-02-23 22:56:09 +0900

    feat: 抽象クラスを使って型のリファクタリング

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -1,14 +1,20 @@
+abstract class Game {
+  abstract setting(): Promise<void>
+  abstract play(): Promise<void>
+  abstract end(): void
+}
+
 const nextActions = ['play again', 'change game', 'exit'] as const
 type NextAction = typeof nextActions[number]
 const gameTitles = ['hit and blow', 'janken'] as const
 type GameTitle = typeof gameTitles[number]
 type GameStore = {
-  [key in GameTitle]: HitAndBlow | Janken
+  [key in GameTitle]: Game
 }
 
 class GameProcedure {
   private currentGameTitle: GameTitle | '' = '';
-  private currentGame: HitAndBlow | Janken | null = null
+  private currentGame: Game | null = null
 
   constructor(private readonly gameStore: GameStore) {
   }
@@ -85,7 +91,7 @@ const promptSelect = async <T extends string>(text: string, values: readonly T[]
 const modes = ['normal', 'hard'] as const
 type Mode = typeof modes[number]
 
-class HitAndBlow {
+class HitAndBlow implements Game {
   private readonly answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
   private answer: string[] = []
   private tryCount = 0
@@ -172,7 +178,7 @@ class HitAndBlow {
 const jankenOptions = ['rock', 'paper', 'scissors'] as const
 type JankenOption = typeof jankenOptions[number]
 
-class Janken {
+class Janken implements Game {
   private rounds = 0
   private currentRound = 1
   private result = {
